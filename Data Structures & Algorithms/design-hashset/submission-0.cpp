a2ea7ab8359113def5db75682c7bc17305c85bb4
Himanshu goyal <himanshugoyal5943@gmail.com>
2026-05-09 07:57:08 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.cpp
@@ -0,0 +1,28 @@
+class MyHashSet {
+public:
+   unordered_map<int,int> mp;
+    MyHashSet() {    
+    }
+    
+    void add(int key) {
+        mp[key] = key;
+    }
+    
+    void remove(int key) {
+        mp.erase(key);
+    }
+    
+    bool contains(int key) {
+        if(mp.find(key)!=mp.end()) return true;
+        return false;
+    }  
+};
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
