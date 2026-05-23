9cb17d7aa7a29e865b83c1d52132994481f43188
Himanshu goyal <himanshugoyal5943@gmail.com>
2026-05-23 11:42:49 +0530

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int count5=0;
+        int count10=0;
+        for(int i=0;i<bills.size();i++){
+          if(bills[i]==5) count5++;
+          else if(bills[i]==10){ if(count5<=0) return false; count5--; count10++;}
+          else {
+            if(count10>0 && count5>0){
+                count5--; count10--;
+            }else if(count5>=3) { count5 = count5-3;}
+            else return false;
+          }
+        }
+        return true;
+    }
+};
