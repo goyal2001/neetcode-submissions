71b7c0e36b5f8336bcb01f26985e9f239bfc73e6
Himanshu goyal <himanshugoyal5943@gmail.com>
2026-06-01 08:09:11 +0530

    Add: house-robber-ii - submission-1

---
+++ Data Structures & Algorithms/house-robber-ii/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int i,int j,vector<int>& dp){
+        if(i>=j) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int skip= solve(nums,i+1,j,dp);
+        int pick = nums[i]+ solve(nums,i+2,j,dp);
+        return dp[i]=max(skip,pick);
+    }
+    int rob(vector<int>& nums) {
+        int j=nums.size();
+        if(j==1) return nums[0];
+        vector<int> dp(nums.size(),-1);
+        int pickFirst = solve(nums,0,j-1,dp);
+        vector<int> dp2(nums.size(),-1);
+        int pickLast = solve(nums,1,j,dp2); 
+    return max(pickFirst,pickLast);
+    }
+};
