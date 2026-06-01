672adbb73a9f522796f060464878cda190224d1f
Himanshu goyal <himanshugoyal5943@gmail.com>
2026-06-01 08:48:05 +0530

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int> dp(n+1,0);
+        if(n>=1) dp[1]=1;
+        if(n>=2) dp[2]=1;
+        if(n<=2) return dp[n];
+        for(int i=3;i<=n;i++){
+            dp[i]= dp[i-3]+dp[i-2]+dp[i-1];
+        }
+        return dp[n];
+    }
+};
