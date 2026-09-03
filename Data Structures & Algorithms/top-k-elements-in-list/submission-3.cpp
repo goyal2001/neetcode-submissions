7056f862b0edd61e15f9c2e1a417cc1dc59e427f
Himanshu goyal <himanshugoyal5943@gmail.com>
2026-09-03 11:15:47 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    struct CompareSecondMin {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        // Returns true if 'a' has a lower priority (is larger) than 'b'
+        return a.second > b.second; 
+    }
+};
+
+struct CompareSecondMax {
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        // Returns true if 'a' has a lower priority than 'b'
+        return a.second < b.second; 
+    }
+};
+
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        vector<int> ans;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, CompareSecondMin> maxPq;
+        unordered_map<int,int> mp;
+        for(int n:nums){
+            if(mp.find(n)!=mp.end()) mp[n]++;
+            else mp[n]=1;
+        }
+        for(auto m:mp){
+            maxPq.push({m.first,m.second});
+            if(maxPq.size()>k) maxPq.pop();
+        }
+        while(k>0){ ans.push_back(maxPq.top().first); maxPq.pop(); k--;}
+        return ans;
+    }
+};
